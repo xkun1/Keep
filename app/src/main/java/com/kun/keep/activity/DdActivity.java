d5d3a1a8e030304293d91c351c5e55baedf59664
kun <kun@kunhe.local>
2018-05-02 18:18:09 +0800

    添加了一些东西，增加卫星定位

--- app/src/main/java/com/kun/keep/activity/DdActivity.java
+++ app/src/main/java/com/kun/keep/activity/DdActivity.java
@@ -1,29 +1,33 @@
 package com.kun.keep.activity;
 
-import android.Manifest;
 import android.annotation.SuppressLint;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.ServiceConnection;
-import android.content.pm.PackageManager;
-import android.os.Build;
-import android.os.Handler;
-import android.os.IBinder;
-import android.os.Message;
-import android.support.annotation.NonNull;
+import android.content.SharedPreferences;
+import android.location.GpsSatellite;
+import android.location.GpsStatus;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.preference.PreferenceManager;
+import android.text.SpannableString;
+import android.text.style.RelativeSizeSpan;
 import android.util.Log;
+import android.view.View;
+import android.widget.Chronometer;
 import android.widget.TextView;
 
-import com.kun.keep.KeepApplicon;
+import com.gc.materialdesign.views.ProgressBarCircularIndeterminate;
+import com.gc.materialdesign.widgets.Dialog;
+import com.google.gson.Gson;
 import com.kun.keep.R;
-import com.kun.keep.confing.Constant;
-import com.kun.keep.greendao.DBdKeepDataDao;
-import com.kun.keep.service.LoactionService;
-import com.kun.keep.utils.ToastUtils;
+import com.kun.keep.grub.Data;
+import com.kun.keep.grub.GpsServices;
+import com.melnykov.fab.FloatingActionButton;
 
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.Locale;
 
 /**
  * user:kun
@@ -31,135 +35,320 @@ import java.util.TimerTask;
  * email:hekun@gamil.com
  * Desc:
  */
-public class DdActivity extends BaseActivity {
+public class DdActivity extends BaseActivity implements LocationListener, GpsStatus.Listener {
 
-    private static final int BAIDU_ACCESS_COARSE_LOCATION = 1;
-
-    private static final String TAG = DdActivity.class.getSimpleName();
-    TextView tv_text;
-    TextView tv_sot;
-    private boolean isBind = false;
-    //是否开始移动
-    private boolean isStart = true;
-    //数据库
-    private DBdKeepDataDao dataDao;
-    //是否暂停刷新UI
-    private volatile boolean isRefreshUI = true;
-    //5秒刷新一次
-    private static final int REFRESH_TIME = 5000;
-
-    private Timer timer = new Timer(true);
-
-
-    @SuppressLint("HandlerLeak")
-    private Handler mHandler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            switch (msg.what) {
-                case Constant.REFRESH_UI:
-                    if (isRefreshUI) {
-                    }
-                    break;
-                default:
-            }
-            super.handleMessage(msg);
-        }
-    };
-    private TimerTask timerTask = new TimerTask() {
-        @Override
-        public void run() {
-            if (isRefreshUI) {
-                Message message = mHandler.obtainMessage();
-                message.what = Constant.REFRESH_UI;
-                mHandler.sendMessage(message);
-            }
-
-        }
-    };
+    private SharedPreferences sharedPreferences;
+    private LocationManager mLocationManager;
+    private static Data data;
+    private FloatingActionButton fab;
+    private FloatingActionButton refresh;
+    private ProgressBarCircularIndeterminate progressBarCircularIndeterminate;
+    private TextView satellite;
+    private TextView status;
+    private TextView accuracy;
+    private TextView currentSpeed;
+    private TextView maxSpeed;
+    private TextView averageSpeed;
+    private TextView distance;
+    private Chronometer time;
+    private Data.onGpsServiceUpdate onGpsServiceUpdate;
+    private boolean firstfix;
 
 
     @Override
     protected void initdata() {
-        myPermissionRequest();
 
     }
 
-    private void myPermissionRequest() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            // 检查是否拥有权限，申请一个（或多个）权限，并提供用于回调返回的获取码（用户定义)
-            if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(new String[]{
-                        Manifest.permission.ACCESS_COARSE_LOCATION
-                }, BAIDU_ACCESS_COARSE_LOCATION);
-            } else {
-                // 已拥有权限，作相应处理（调用定位SDK应当确保相关权限均被授权，否则可能引起定位失败）
-                startBdService();
-            }
-        } else {
-            // 安卓手机版本在5.0时，配置清单中已申明权限，作相应处理，此处正对sdk版本低于23的手机
-            startBdService();
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            // requestCode即所声明的权限获取码，在checkSelfPermission时传入
-            case BAIDU_ACCESS_COARSE_LOCATION:
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // 第一次获取到权限，请求定位
-                    startBdService();
-                } else {
-                    ToastUtils.showToast("请打开定位权限", DdActivity.this);
-                    // 没有获取到权限，做特殊处理
-                    Log.i("=========", "请求权限失败");
-                }
-                break;
-            default:
-                break;
-        }
-    }
 
     @Override
     protected void initView() {
-        tv_text = findViewById(R.id.tv_text);
-        tv_sot = findViewById(R.id.tv_vot);
-        dataDao = KeepApplicon.getInstances().getDaoSession().getDBdKeepDataDao();
-    }
+        data = new Data(onGpsServiceUpdate);
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setVisibility(View.INVISIBLE);
 
-    private void startBdService() {
-        Intent intent = new Intent(this, LoactionService.class);
-        isBind = bindService(intent, conn, Context.BIND_AUTO_CREATE);
-        startService(intent);
-    }
-
-    ServiceConnection conn = new ServiceConnection() {
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder service) {
-            LoactionService stepService = ((LoactionService.StepBinder) service).getService();
-            stepService.setDis(new LoactionService.Dis() {
-                @Override
-                public void discate(double dis) {
-                    tv_text.setText("当前路程=" + dis);
-                    tv_sot.setText("当前速度=" + dis / 1000);
+        refresh = (FloatingActionButton) findViewById(R.id.refresh);
+        refresh.setVisibility(View.INVISIBLE);
+        onGpsServiceUpdate = new Data.onGpsServiceUpdate() {
+            @Override
+            public void update() {
+                double maxSpeedTemp = data.getMaxSpeed();
+                double distanceTemp = data.getDistance();
+                double averageTemp;
+                if (sharedPreferences.getBoolean("auto_average", false)) {
+                    averageTemp = data.getAverageSpeedMotion();
+                } else {
+                    averageTemp = data.getAverageSpeed();
                 }
-            });
+                String speedUnits;
+                String distanceUnits;
+                if (sharedPreferences.getBoolean("miles_per_hour", false)) {
+                    maxSpeedTemp *= 0.62137119;
+                    distanceTemp = distanceTemp / 1000.0 * 0.62137119;
+                    averageTemp *= 0.62137119;
+                    speedUnits = "mi/h";
+                    distanceUnits = "mi";
+                } else {
+                    speedUnits = "km/h";
+                    if (distanceTemp <= 1000.0) {
+                        distanceUnits = "m";
+                    } else {
+                        distanceTemp /= 1000.0;
+                        distanceUnits = "km";
+                    }
+                }
+                SpannableString s = new SpannableString(String.format("%.0f", maxSpeedTemp) + speedUnits);
+                s.setSpan(new RelativeSizeSpan(0.5f), s.length() - 4, s.length(), 0);
+                maxSpeed.setText(s);
+
+                s = new SpannableString(String.format("%.0f", averageTemp) + speedUnits);
+                s.setSpan(new RelativeSizeSpan(0.5f), s.length() - 4, s.length(), 0);
+                averageSpeed.setText(s);
+
+                s = new SpannableString(String.format("%.3f", distanceTemp) + distanceUnits);
+                s.setSpan(new RelativeSizeSpan(0.5f), s.length() - 2, s.length(), 0);
+                distance.setText(s);
+            }
+        };
+        mLocationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        satellite = (TextView) findViewById(R.id.satellite);
+        status = (TextView) findViewById(R.id.status);
+        accuracy = (TextView) findViewById(R.id.accuracy);
+        maxSpeed = (TextView) findViewById(R.id.maxSpeed);
+        averageSpeed = (TextView) findViewById(R.id.averageSpeed);
+        distance = (TextView) findViewById(R.id.distance);
+        time = (Chronometer) findViewById(R.id.time);
+        currentSpeed = (TextView) findViewById(R.id.currentSpeed);
+        progressBarCircularIndeterminate = (ProgressBarCircularIndeterminate) findViewById(R.id.progressBarCircularIndeterminate);
+        time.setText("00:00:00");
+        time.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            boolean isPair = true;
+
+            @Override
+            public void onChronometerTick(Chronometer chrono) {
+                long time;
+                if (data.isRunning()) {
+                    time = SystemClock.elapsedRealtime() - chrono.getBase();
+                    data.setTime(time);
+                } else {
+                    time = data.getTime();
+                }
+
+                int h = (int) (time / 3600000);
+                int m = (int) (time - h * 3600000) / 60000;
+                int s = (int) (time - h * 3600000 - m * 60000) / 1000;
+                String hh = h < 10 ? "0" + h : h + "";
+                String mm = m < 10 ? "0" + m : m + "";
+                String ss = s < 10 ? "0" + s : s + "";
+                chrono.setText(hh + ":" + mm + ":" + ss);
+
+                if (data.isRunning()) {
+                    chrono.setText(hh + ":" + mm + ":" + ss);
+                } else {
+                    if (isPair) {
+                        isPair = false;
+                        chrono.setText(hh + ":" + mm + ":" + ss);
+                    } else {
+                        isPair = true;
+                        chrono.setText("");
+                    }
+                }
+
+            }
+        });
+    }
+
+    public void onFabClick(View v) {
+        if (!data.isRunning()) {
+            fab.setImageDrawable(getResources().getDrawable(R.drawable.ic_action_pause));
+            data.setRunning(true);
+            time.setBase(SystemClock.elapsedRealtime() - data.getTime());
+            time.start();
+            data.setFirstTime(true);
+            startService(new Intent(getBaseContext(), GpsServices.class));
+            refresh.setVisibility(View.INVISIBLE);
+        } else {
+            fab.setImageDrawable(getResources().getDrawable(R.drawable.ic_action_play));
+            data.setRunning(false);
+            status.setText("");
+            stopService(new Intent(getBaseContext(), GpsServices.class));
+            refresh.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void onRefreshClick(View v) {
+        resetData();
+        stopService(new Intent(getBaseContext(), GpsServices.class));
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    protected void onResume() {
+        super.onResume();
+        firstfix = true;
+        if (!data.isRunning()) {
+            Gson gson = new Gson();
+            String json = sharedPreferences.getString("data", "");
+            data = gson.fromJson(json, Data.class);
+        }
+        if (data == null) {
+            data = new Data(onGpsServiceUpdate);
+        } else {
+            data.setOnGpsServiceUpdate(onGpsServiceUpdate);
         }
 
-        @Override
-        public void onServiceDisconnected(ComponentName name) {
-
+        if (mLocationManager.getAllProviders().indexOf(LocationManager.GPS_PROVIDER) >= 0) {
+            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 500, 0, this);
+        } else {
+            Log.w("MainActivity", "No GPS location provider found. GPS data display will not be available.");
         }
-    };
+
+        if (!mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            showGpsDisabledDialog();
+        }
+
+        mLocationManager.addGpsStatusListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mLocationManager.removeUpdates(this);
+        mLocationManager.removeGpsStatusListener(this);
+        SharedPreferences.Editor prefsEditor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(data);
+        prefsEditor.putString("data", json);
+        prefsEditor.apply();
+    }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if (isBind) {
-            this.unbindService(conn);
+        stopService(new Intent(getBaseContext(), GpsServices.class));
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (location.hasAccuracy()) {
+            SpannableString s = new SpannableString(String.format("%.0f", location.getAccuracy()) + "m");
+            s.setSpan(new RelativeSizeSpan(0.75f), s.length() - 1, s.length(), 0);
+            accuracy.setText(s);
+
+            if (firstfix) {
+                status.setText("");
+                fab.setVisibility(View.VISIBLE);
+                if (!data.isRunning() && !maxSpeed.getText().equals("")) {
+                    refresh.setVisibility(View.VISIBLE);
+                }
+                firstfix = false;
+            }
+        } else {
+            firstfix = true;
+        }
+
+        if (location.hasSpeed()) {
+            progressBarCircularIndeterminate.setVisibility(View.GONE);
+            String speed = String.format(Locale.ENGLISH, "%.0f", location.getSpeed() * 3.6) + "km/h";
+
+            if (sharedPreferences.getBoolean("miles_per_hour", false)) { // Convert to MPH
+                speed = String.format(Locale.ENGLISH, "%.0f", location.getSpeed() * 3.6 * 0.62137119) + "mi/h";
+            }
+            SpannableString s = new SpannableString(speed);
+            s.setSpan(new RelativeSizeSpan(0.25f), s.length() - 4, s.length(), 0);
+            currentSpeed.setText(s);
+        }
+
+    }
+
+    @Override
+    public void onGpsStatusChanged(int event) {
+        switch (event) {
+            case GpsStatus.GPS_EVENT_SATELLITE_STATUS:
+                @SuppressLint("MissingPermission")
+                GpsStatus gpsStatus = mLocationManager.getGpsStatus(null);
+                int satsInView = 0;
+                int satsUsed = 0;
+                Iterable<GpsSatellite> sats = gpsStatus.getSatellites();
+                for (GpsSatellite sat : sats) {
+                    satsInView++;
+                    if (sat.usedInFix()) {
+                        satsUsed++;
+                    }
+                }
+                satellite.setText(String.valueOf(satsUsed) + "/" + String.valueOf(satsInView));
+                if (satsUsed == 0) {
+                    fab.setImageDrawable(getResources().getDrawable(R.drawable.ic_action_play));
+                    data.setRunning(false);
+                    status.setText("");
+                    stopService(new Intent(getBaseContext(), GpsServices.class));
+                    fab.setVisibility(View.INVISIBLE);
+                    refresh.setVisibility(View.INVISIBLE);
+                    accuracy.setText("");
+                    status.setText(getResources().getString(R.string.waiting_for_fix));
+                    firstfix = true;
+                }
+                break;
+
+            case GpsStatus.GPS_EVENT_STOPPED:
+                if (!mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                    showGpsDisabledDialog();
+                }
+                break;
+            case GpsStatus.GPS_EVENT_FIRST_FIX:
+                break;
+            default:
         }
     }
+
+    public void showGpsDisabledDialog() {
+        Dialog dialog = new Dialog(this, getResources().getString(R.string.gps_disabled), getResources().getString(R.string.please_enable_gps));
+
+        dialog.setOnAcceptButtonClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent("android.settings.LOCATION_SOURCE_SETTINGS"));
+            }
+        });
+        dialog.show();
+    }
+
+    public void resetData() {
+        fab.setImageDrawable(getResources().getDrawable(R.drawable.ic_action_play));
+        refresh.setVisibility(View.INVISIBLE);
+        time.stop();
+        maxSpeed.setText("");
+        averageSpeed.setText("");
+        distance.setText("");
+        time.setText("00:00:00");
+        data = new Data(onGpsServiceUpdate);
+    }
+
+    public static Data getData() {
+        return data;
+    }
+
+//    @Override
+//    public void onBackPressed() {
+//        Intent a = new Intent(Intent.ACTION_MAIN);
+//        a.addCategory(Intent.CATEGORY_HOME);
+//        a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//        startActivity(a);
+//    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+    }
+
 
     @Override
     protected int setLayoutId() {
